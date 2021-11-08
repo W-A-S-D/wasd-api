0040d9d37c20c2f05cb22807068a9d0faa736b34
BiaVediner <bianca.fraccaroli@bandtec.com.br>
2021-11-08 14:20:40 -0300

    Adding Create Company Service and Controller

---
+++ src/controllers/CreateCompanyController.js
@@ -0,0 +1,19 @@
+let CreateCompanyService = require('../services/CreateCompanyService');
+
+class CreateUserController {
+    async hCompanye(request, response) {
+        const { nome, email, cnpj, logradouro, numero, bairro, cidade, uf, cep, telefone } = request.body;
+
+        const service = new CreateCompanyService();
+
+        try {
+            const result = await service.execute(nome, email, cnpj, logradouro, numero, bairro, cidade, uf, cep, telefone);
+
+            return response.json(result);
+        } catch (error) {
+            return response.json({ error: error.message })
+        }
+    }
+}
+
+module.exports = CreateCompanyController;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let router = express.Router();
 const CreateUserController = require("./controllers/CreateUserController");
+const CreateCompanyController = require("./controllers/CreateCompanyController");
 const AuthenticateUserController = require("./controllers/AuthenticateUserController");
 const ensureAuthenticated = require("./middleware/ensureAuthenticated");
 
@@ -9,6 +10,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/create', new CreateUserController().handle)
+router.post('/create-company', new CreateCompanyController().handle)
 router.post('/authenticate', new AuthenticateUserController().handle)
 
 module.exports = router;
