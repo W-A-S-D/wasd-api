66be23d75b9fd40d0aef229da4e3066e7ed3be0e
Manocchio <guruu.leste@gmail.com>
2021-11-14 14:59:55 -0300

    sectors with user relationship

--- src/services/SectorService/ListSectorsService.js
+++ src/services/SectorService/ListSectorsService.js
@@ -3,7 +3,18 @@ let prisma = require("../../prisma")
 
 class ListSectorsService {
     execute = async() => {
-        const sectorsList = await prisma.setor.findMany();
+        const sectorsList = await prisma.setor.findMany({
+            select: {
+                setor_id: true,
+                jogo: true,
+                avatar_jogo: true,
+                usuario: {
+                    select: {
+                        nome: true,
+                    }
+                }
+            }
+        })
 
         if (!sectorsList) {
             throw new Error("Impossible to reach")
