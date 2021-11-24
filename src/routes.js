9d69b311b30080cec5e220c9ed7548fee11977dd
Bianca Vediner <79323910+BiaVediner@users.noreply.github.com>
2021-11-24 12:11:20 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -39,7 +39,10 @@ const ListDiscoByMachineController = require("./controllers/MachineController/Li
 const CreateFuncController = require("./controllers/UserController/CreateFuncController");
 
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.status(200).send({
+    title: "Node Express API",
+    version: "0.0.1"
+  });
 });
 
 //users
