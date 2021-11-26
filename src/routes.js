da55e06ef142af94249f5b682925dc8ec4670a53
Manocchio <guruu.leste@gmail.com>
2021-11-26 18:56:45 -0300

    List Users Without Sector

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,19 @@ let router = express.Router();
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+    destination: function(req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    },
 });
 
 const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  }
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    }
 });
 const ensureAuthenticated = require("./middleware/ensureAuthenticated");
 
@@ -28,6 +28,7 @@ const ListMachinesBySectorController = require("./controllers/MachineController/
 const ListMachinesByStatusController = require("./controllers/MachineController/ListMachinesByStatusController");
 const CreateSectorController = require("./controllers/SectorController/CreateSectorController");
 const ListUsersController = require("./controllers/UserController/ListUsersController");
+const ListUsersWithoutSector = require("./controllers/UserController/ListUsersWithoutSectorController");
 const ListSectorsByCompanyController = require("./controllers/SectorController/ListSectorsByCompanyController");
 const DeleteUserController = require("./controllers/UserController/DeleteUserController");
 const UpdateUserController = require("./controllers/UserController/UpdateUserController");
@@ -38,78 +39,83 @@ const ListLogDiscoByLogController = require("./controllers/LogController/ListLog
 const ListDiscoByMachineController = require("./controllers/MachineController/ListDiscoByMachinesController");
 const CreateFuncController = require("./controllers/UserController/CreateFuncController");
 
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", function(req, res, next) {
+    res.render("index", { title: "Express" });
 });
 
 //users
 router.get("/users", new ListUsersController().handle);
 router.get(
-  "/users/company",
-  ensureAuthenticated,
-  new ListUsersByCompanyController().handle
+    "/users/company",
+    ensureAuthenticated,
+    new ListUsersByCompanyController().handle
 );
 router.get("/user", ensureAuthenticated, new FindUserByIdController().handle);
 router.post("/create", new CreateUserController().handle);
+router.get('/users/nosector', new ListUsersWithoutSector().handle);
 router.post(
-  "/create/func",
-  ensureAuthenticated,
-  new CreateFuncController().handle
+    "/create/func",
+    ensureAuthenticated,
+    new CreateFuncController().handle
 );
 router.post("/update", ensureAuthenticated, new UpdateUserController().handle);
 router.post("/authenticate", new AuthenticateUserController().handle);
 router.delete(
-  "/delete/:idUser",
-  ensureAuthenticated,
-  new DeleteUserController().handle
+    "/delete/:idUser",
+    ensureAuthenticated,
+    new DeleteUserController().handle
 );
 
 // sectors
 router.get("/sectors", new ListSectorsController().handle);
 router.get(
-  "/sectors/user",
-  ensureAuthenticated,
-  new FindSectorByUserController().handle
+    "/sectors/user",
+    ensureAuthenticated,
+    new FindSectorByUserController().handle
 );
 router.get(
-  "/sectors/company",
-  ensureAuthenticated,
-  new ListSectorsByCompanyController().handle
+    "/sectors/company",
+    ensureAuthenticated,
+    new ListSectorsByCompanyController().handle
 );
 
+
+
 router.post(
-  "/sectors/create",
-  upload.single("productImage"),
-  ensureAuthenticated,
-  new CreateSectorController().handle
+    "/sectors/create",
+    upload.single("productImage"),
+    ensureAuthenticated,
+    new CreateSectorController().handle
 );
+
+
 
 //machines
 router.get(
-  "/machines/sector/:idSetor",
-  new ListMachinesBySectorController().handle
+    "/machines/sector/:idSetor",
+    new ListMachinesBySectorController().handle
 );
 router.get(
-  "/machines/",
-  ensureAuthenticated,
-  new ListMachinesByStatusController().handle
+    "/machines/",
+    ensureAuthenticated,
+    new ListMachinesByStatusController().handle
 );
 router.get(
-  "/discos/:idMaquina",
-  ensureAuthenticated,
-  new ListDiscoByMachineController().handle
+    "/discos/:idMaquina",
+    ensureAuthenticated,
+    new ListDiscoByMachineController().handle
 );
 
 //log
 router.get(
-  "/log/:idMaquina",
-  ensureAuthenticated,
-  new ListLogByMachineController().handle
+    "/log/:idMaquina",
+    ensureAuthenticated,
+    new ListLogByMachineController().handle
 );
 router.get(
-  "/logDisco/:idLog",
-  ensureAuthenticated,
-  new ListLogDiscoByLogController().handle
+    "/logDisco/:idLog",
+    ensureAuthenticated,
+    new ListLogDiscoByLogController().handle
 );
 
 //company
---
+++ src/services/UserService/ListUsersWithoutSectorService.js
@@ -0,0 +1,28 @@
+let prisma = require("../../prisma")
+
+
+class ListUsersWithoutSectorService {
+    execute = async() => {
+        const usersList = await prisma.usuario.findMany({
+            select: {
+                usuario_id: true,
+                nome: true,
+            },
+            where: {
+                setor: {
+                    none: {},
+                }
+            }
+        });
+
+        if (!usersList) {
+            throw new Error("Impossible to reach");
+        }
+
+        return usersList;
+
+    }
+}
+
+
+module.exports = ListUsersWithoutSectorService;
